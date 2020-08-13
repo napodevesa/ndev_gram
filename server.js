4f3061ec141bf52a855e5c281c085c2144a4cab3
ndev <napodevesa@gmail.com>
2020-08-13 19:09:57 +0200

    node server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-var express = requiere ("express");
+var express = require ("express");
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(3000,function(err) {
+	if (err) 
+		return console.log("error"), process.exit(1);
+	console.log ("ndevgram en el puerto 300");
+})
